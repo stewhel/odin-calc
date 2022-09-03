9efc4b649d221b7e7661d2148267ee1c32476301
Stephen H <stewhel9@gmail.com>
2022-09-02 20:51:14 -0400

    Fixed calcuclations when switching operators

--- javascript.js
+++ javascript.js
@@ -19,6 +19,9 @@ let display = document.getElementById('screen')
 let newCalc = true
 let operator = undefined
 let nums = []
+let switchcalc = false
+let lastKey = false
+let lastOper = false
 
 operators = {
     "+" : plus,
@@ -30,52 +33,91 @@ operators = {
 }
 
 function typeNum(e){
+    if(isNaN(display.textContent)){
+        display.textContent = ""
+        nums = []
+    }
     if(newCalc == true){
         display.textContent = ""
         newCalc = false
-        console.log(newCalc)
+    }else if(nums.length == 2){
+        nums.shift()
+        nums.push(+display.textContent)
+        display.textContent = ""
     }
     display.textContent += e.srcElement.innerText;
-    round();
+    lastKey = false;
 }
 
 function typeOper(e){  
-    operator = operators[e.srcElement.innerText];
-    nums.push(+display.textContent);
-    display.textContent = ""
-    if(nums.length == 2){
-        display.textContent = operator(nums[1], nums[2])
-        round()
-    }if(nums.length == 3){
-        nums.shift()
-        nums.shift()
+    if(+display.textContent == 0){
+        return
     }
+    if(switchcalc == true){
+        nums = []
+        switchcalc = false
+    }
+
+    nums.push(+display.textContent);
+
+    if(nums.length == 1){
+        operator = operators[e.srcElement.innerText];
+        display.textContent = "" 
+    }else if(nums.length == 2){
+        display.textContent = operator(nums[0], nums[1]);
+        nums.push(display.textContent)
+        nums.shift()
+        operator = operators[e.srcElement.innerText];
+    }else if(nums.length == 3){
+        display.textContent = operator(nums[1], nums[2]);
+        nums.shift()
+        operator = operators[e.srcElement.innerText];
+    }
+
     newCalc = false
+    lastKey = true
+    round()
 }
 
 function equals(){   
+
+    if(nums.length == 0){
+        return
+    }
     nums.push(+display.textContent);
     if(nums.length == 2){
         display.textContent = operator(nums[0], nums[1])
+        switchcalc = true
         round()
     }else if(nums.length == 3){
         display.textContent = operator(nums[1], nums[2])
-        round()
         nums.pop()
+        switchcalc = true
+        round()
     }
     newCalc = true
+    lastKey = false
+    round()
 }
 
 function round(){
     if (+display.textContent.length > 8){
         display.textContent = display.textContent.slice(0,-1)
+        display.textContent = Math.round(+display.textContent * 10)/10
+    }else if(+display.textContent == 0){
+        return
+    }else{
+     display.textContent = Math.round(+display.textContent * 10)/10   
     }
-    display.textContent = Math.round(+display.textContent * 10)/10
 }
 
-function clear(){display.textContent = ""}
+function clear(){
+    display.textContent = ""
+    operator = null;
+    nums = []}
+
 function del(){ display.textContent = display.textContent.slice(0,-1)}
-function plus(a, b){return a + b}
+function plus(a, b){ return (a + b)}
 function minus(a, b){return a - b}
 function divide(a, b){return a / b}
 function multiply(a, b){return a * b}
